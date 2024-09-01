054a1c579932eec4275172b68c22e0a3b814e4f9
Anthony Candelino <anthcandelino@gmail.com>
2024-08-31 22:03:18 -0400

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Anthony Candelino on 2024-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
